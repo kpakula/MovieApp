035b4cefb422befb82e67183f7a6b5b55112d455
Konrad Pakula <konrad.a.pakula@gmail.com>
2020-02-28 08:42:52 +0100

    Handle state

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchBar from './components/SearchBar';
 import {apiUrl} from "./utils/Api"
 function App() {
+  const [state, setState] = useState({
+    search: "",
+    results: [],
+    selected: {}
+  });
+
+  const handleInput = (event) => {
+    const currentSearch = event.target.value;
+
+    setState(prevState => {
+      return {...prevState, search: currentSearch}
+    });
+
+    console.log(state.s);
+  }
+
+
+
+
   return (
     <div className="App">
       <header>
