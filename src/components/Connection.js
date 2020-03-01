bcfbb327a073caf66086c41fcffb2d63c232931e
Konrad Pakula <konrad.a.pakula@gmail.com>
2020-03-01 14:49:31 +0100

    Style connection info component

--- src/components/Connection.js
+++ src/components/Connection.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+
+import "./Connection.css";
 
 function Connection() {
-    return (
-        <div className="">
-          <h2>Something goes wrong...</h2>
-        </div>
-    )
+  return (
+    <div className="connection-info">
+      <div className="connection-info-inner">
+        <h2>Something goes wrong... :(</h2>
+      </div>
+    </div>
+  );
 }
 
-export default Connection
+export default Connection;
