076dae4cdd8da6b870a2f704d28171ddb58945ae
Konrad Pakula <konrad.a.pakula@gmail.com>
2020-03-03 17:45:13 +0100

    Added Bootstrap and refactored view

--- src/components/movies/Movie.js
+++ src/components/movies/Movie.js
@@ -3,7 +3,7 @@ import React from 'react'
 import './Movie.css'
 function Movie({ movie, openPopup }) {
     return (
-        <div className="movie" onClick={() => openPopup(movie.imdbID)}>
+        <div className="col-12 col-sm-6 col-lg-4 movie" onClick={() => openPopup(movie.imdbID)}>
             <img src={movie.Poster} alt={movie.Title + " cover image"}/>
             <h3>{movie.Title}</h3>
         </div>
--- src/components/movies/Movies.js
+++ src/components/movies/Movies.js
@@ -4,20 +4,15 @@ import "./Movies.css";
 
 function Movies({ movies, openPopup }) {
   return (
-    <section className="movies">
-      <div className="container-fluid text-center">
-        <div class="row">
-          <div class="col-12 col-sm-6 col-lg-4">One of three columns</div>
-          <div class="col-12 col-sm-6 col-lg-4">One of three columns</div>
-          <div class="col-12 col-sm-6 col-lg-4">One of three columns</div>
+      <div className="container-fluid text-center movies">
+        <div class="row justify-content-center">
+          {!movies.length
+            ? null
+            : movies.map(movie => (
+                <Movie key={movie.imdbID} movie={movie} openPopup={openPopup} />
+              ))}
         </div>
       </div>
-      {!movies.length
-        ? null
-        : movies.map(movie => (
-            <Movie key={movie.imdbID} movie={movie} openPopup={openPopup} />
-          ))}
-    </section>
   );
 }
 
