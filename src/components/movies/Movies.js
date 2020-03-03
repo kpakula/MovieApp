e05cf66e71af72382ae3a84bcb9ee1c493e75381
Konrad Pakula <konrad.a.pakula@gmail.com>
2020-03-03 13:15:45 +0100

    Test component

--- src/components/movies/Movies.js
+++ src/components/movies/Movies.js
@@ -1,13 +1,22 @@
 import React from "react";
 import Movie from "./Movie";
-import "./Movies.css"
+import "./Movies.css";
 
 function Movies({ movies, openPopup }) {
   return (
     <section className="movies">
+      <div className="container-fluid text-center">
+        <div class="row">
+          <div class="col-12 col-sm-6 col-lg-4">One of three columns</div>
+          <div class="col-12 col-sm-6 col-lg-4">One of three columns</div>
+          <div class="col-12 col-sm-6 col-lg-4">One of three columns</div>
+        </div>
+      </div>
       {!movies.length
         ? null
-        : movies.map(movie => <Movie key={movie.imdbID} movie={movie} openPopup={openPopup} />)}
+        : movies.map(movie => (
+            <Movie key={movie.imdbID} movie={movie} openPopup={openPopup} />
+          ))}
     </section>
   );
 }
