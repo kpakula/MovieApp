02474597bc9c6767e2227ea7417ae64f3178602f
Konrad Pakula <konrad.a.pakula@gmail.com>
2020-03-04 12:23:53 +0100

    Modified card info view

--- src/components/movies/Movie.js
+++ src/components/movies/Movie.js
@@ -13,28 +13,17 @@ function Movie({ movie, openPopup }) {
       onClick={() => openPopup(movie.imdbID)}
     >
       <div class="card">
-        <img class="card-img-top" src={movie.Poster} alt={movie.Title + " cover image"} />
+        <img
+          class="card-img-top"
+          src={movie.Poster}
+          alt={movie.Title + " cover image"}
+        />
         <div class="card-body">
-          <h5 class="card-title">{movie.Title}</h5>
-          <p class="card-text text-truncate">
-            This is a longer card with supporting text below as a natural
-            lead-in to additional content. This content is a little bit longer.
-          </p>
-          <p class="card-text">
-            <small class="text-muted">Last updated 3 mins ago</small>
-          </p>
+          <h5 class="card-title">
+            {movie.Title} - {movie.Year}
+          </h5>
         </div>
       </div>
-
-      {/* <div className="movie">
-        <div className="movie-data">
-        <img
-          src={movie.Poster}
-          alt={splitTitleIfTooLong(movie.Title) + " cover image"}
-        />
-        <h3>{movie.Title}</h3>
-        </div>
-      </div> */}
     </div>
   );
 }
